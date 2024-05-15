4512733d65416aad4480ba7a5dd1bdf986d049b0
Ankit Sharma <ankitsh474@gmail.com>
2024-05-15 17:49:55 +0530

    image of more details page fixed

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ import authRoutes from './routes/authRoute.js';
 import cors from "cors"
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-import mongoose from "mongoose";
-
 
 dotenv.config();
 
@@ -20,7 +18,6 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static('public'));
-
 
 // routes
 app.use('/api/v1/auth',authRoutes);
